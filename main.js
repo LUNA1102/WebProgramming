9b2ecde68ac9f7e27a5d5a7d376786a35ffd8e55
이정민 <moong3u@naver.com>
2022-11-27 16:35:15 +0900

    1127

--- main.js
+++ main.js
@@ -1,29 +1,66 @@
-const form = document.querySelector("task-form");
-const input = document.querySelector("#tast-input");
-const list = document.querySelector("#tasks");
+const form = document.querySelector("#new-task-form");
+const input = document.querySelector("#new-task-input");
+const list_el = document.querySelector("#tasks");
 
-form.addEventListener("submit", function(e) {
+form.addEventListener("submit", function (e) {
     e.preventDefault();
+    // submit 이벤트가 발생하면 페이지가 새로고침됨으로 모든 정보가 사라짐
+    // 그 동작을 중지시키는 것이 preventDefault()
 
     const task = input.value;
-
     if (!task) {
-        // 할 일이 입력창에 내용이 입력되지 않았으면
-        alert("할 일을 입력하세요."); // 경고창 발생
-    }
-
-    else {
+        // 할 일 입력창에 내용이 입력되지 않았으면
+        alert("할 일을 입력하세요"); // 경고창 발생
+    } else {
         const task_el = document.createElement("div");
         // createElement(): 문서에 HTML 요소 추가
         task_el.classList.add("task");
-        // classList.add(): 지정한 클래스 값을 추가
+        // classLlist.add(): 지정한 클래스 값을 추가
         const task_content_el = document.createElement("div");
         task_content_el.classList.add("content");
 
         task_el.appendChild(task_content_el); // task > content
-        // 선택한 요소 안에 자식요소를 추가
-        // append와 appendChild의 차이점
-        // append(): 노드 객체나 text를 받을 수 있고, 한 번에 여러 개의 자식 요소 설정 가능
-        // appendChild(): 오직 노드 객체만 받을 수 있음
+        // appendChild(): 선택한 요소 안에 자식 요소 추가
+
+
+        const task_input_el = document.createElement("input");
+        task_input_el.classList.add("text");
+        task_input_el.type = "text";
+        task_input_el.value = task;
+        task_input_el.setAttribute("readonly", "readonly");
+        // setAttribute('속성 이름', '속성값');
+        // input 자체에서 수정할 수 없게 하기
+
+        task_content_el.appendChild(task_input_el); // content > input
+
+        const task_actions_el = document.createElement("div");
+        task_actions_el.classList.add("actions");
+
+        const task_edit_el = document.createElement("button");
+        task_edit_el.classList.add("edit");
+        task_edit_el.innerText = "Edit";
+
+        const task_complete_el = document.createElement("button");
+        task_complete_el.classList.add("complete");
+        task_complete_el.innerText = "Complete";
+
+        const task_delete_el = document.createElement("button");
+        task_delete_el.classList.add("delete");
+        task_delete_el.innerText = "Delete";
+
+        task_actions_el.appendChild(task_edit_el); // actions > edit
+        task_actions_el.appendChild(task_complete_el); // actions > complete
+        task_actions_el.appendChild(task_delete_el); // actions > delete
+
+        task_el.appendChild(task_actions_el); // task > actions
+        list_el.appendChild(task_el); // list > task
+
+        input.value = ""; // 할 일이 입력된 경우 할 일 입력칸을 비움
+
+        task_complete_el.addEventListener("click", function () {
+        task_input_el.classList.toggle("completed");
+        // 할 일 완료하면 줄 긋기
+        // toggle() -> ON/OFF
+        });
     }
 });
